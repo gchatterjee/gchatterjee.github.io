71e488dca34f56439e8997879254020185a663f9
gchatterjee <chatterjeegaur@gmail.com>
2021-03-07 16:54:12 -0500

    fade in effect/

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,12 @@ const path = require('path')
 module.exports = {
   devtool: 'source-map',
   mode: 'production',
+  entry: {
+    index: './src/index.js',
+    postload: './src/postload.js',
+  },
   output: {
     path: path.resolve(__dirname, 'docs'),
-    filename: 'index.js',
+    filename: '[name].js',
   },
 }
